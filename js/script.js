f4a56fdae514be7c529afb07a240a5f024e64d94
DaviSunrider <davisunrider@gmail.com>
2024-05-02 23:15:45 -0300

    add trasnferencia de informação via url

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//ativar links do navbar
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +11,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+//ativar itens orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  elemento.checked = true;
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
